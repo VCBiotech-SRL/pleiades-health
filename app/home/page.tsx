d2bf4297bcb95a56a9ecb1c895abe4aec1812956
Victor Calderon <victorcalderon@protonmail.com>
2023-07-05 08:08:18 -0400

    I added shadcn UI button and configs

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,11 @@
+import { buttonVariants } from "@/components/ui/button";
+import { getSession } from "@/lib/auth";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const session = await getSession();
   return (
     <div className="flex h-screen bg-black">
       <div className="m-auto w-48">
@@ -10,6 +15,12 @@ export default function HomePage() {
           src="/logo.png"
           alt="VCBiotech Health Community"
         />
+        <Link
+          href="/login"
+          className={cn(buttonVariants({ variant: "default" }), "w-full")}
+        >
+          {session ? "Go to Dashboard" : "Sign in"}
+        </Link>
       </div>
     </div>
   );
