7516968fc0c005aaf771a81e4a62d52bbcf58251
Victor Calderon <victorcalderon@protonmail.com>
2023-07-07 17:52:17 -0400

    I removed the dark invert

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -14,7 +14,7 @@ export default async function HomePage() {
           height={512}
           src="/logo.png"
           alt="VCBiotech Health Community"
-          className="dark:invert"
+          className=""
         />
         <Link
           href={`/login`}
